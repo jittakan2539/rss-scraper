41fbfb4406ddc48decf93858e17a13f8866b1e0b
Kan Jittapramoulboon <kan.j@themather.asia>
2025-01-15 17:04:41 +0700

    create json.go and handler_readiness.go

---
+++ handler_readiness.go
@@ -0,0 +1,3 @@
+package main
+
+handleReadiness(w http.ResponseWriter, r *http.Request)
--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+
 
 func main() {
-	fmt.Println("Hello World")
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment.")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.HandleFunc("/ready", handleReadiness)
+
+	router.Mount("/v1", v1Router)
+
+	router.Use()
+
+	server := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("PORT running on ", portString)
 }
