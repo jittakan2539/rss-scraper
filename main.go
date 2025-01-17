ec7ee4881872a052f9b13d3bf4f3d715e0236858
kanjitta <jittakan2539@gmail.com>
2025-01-17 17:52:26 +0700

    Add getUser endpoint in main.go

--- main.go
+++ main.go
@@ -58,7 +58,9 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerError)
+	//---------------User endpoint---------------//
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.handlerGetUser)
 
 	router.Mount("/v1", v1Router)
 
