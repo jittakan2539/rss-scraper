2ada3551f967920a00d92f7d712943f8c205e277
kanjitta <jittakan2539@gmail.com>
2025-01-17 17:50:02 +0700

    create HandlerGetUser in handler_user.go

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jittakan2539/rss-scraper/internal/auth"
 	"github.com/jittakan2539/rss-scraper/internal/database"
 )
 
@@ -35,9 +36,21 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 	
-	respondWithJSON(w, 200, databaseUserToUser((user)))
+	respondWithJSON(w, 201, databaseUserToUser((user)))
 }
 
 func (apiCfg *apiConfig) handlerGetUser (w http.ResponseWriter, r *http.Request) {
-	
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %s", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databaseUserToUser((user)))
 }
